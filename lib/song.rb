89c2ead9b9a04c9b67bc9936210b4babffbf009d
Derek Cerretani <derekcerretani@gmail.com>
2019-02-05 19:22:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,8 @@ class Song
 
   def self.genre_count
     genre_hash = {}
-    @@genres.each do |genre|
-      genre_hash[genre] = " "
+    self.genres.each do |genre|
+      genre_hash[genre] = 
     end
     genre_hash
     binding.pry
