aa30100094f8117e25386f865e4bed0c8caedd1d
Derek Cerretani <derekcerretani@gmail.com>
2019-02-05 19:08:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   end
 
   def self.genre_count
-    hash = Hash[self.genres.collect { |genre| [genre, "" ] } ]
+    hash = Hash[self.genres.collect { |genre| [genre, "#{genre.count}" ] } ]
     binding.pry
   end
 
