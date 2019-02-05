b1c32da263598d27cba6fa2346ff154bfcb57958
Derek Cerretani <derekcerretani@gmail.com>
2019-02-05 18:22:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,14 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@artists = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists << @artist
   end
 
   def self.count
@@ -16,7 +18,7 @@ class Song
   end
 
   def self.artists
-
+    @@artists
   end
 
 end
